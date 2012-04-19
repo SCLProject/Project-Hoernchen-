bbdb736a70d6112d29fff8a7b7eb0f63b89c9b51
nitaka0114 <nitaka.masa@gmail.com>
2012-04-20 03:27:01 +0900

    1st commit とりあえずプロジェクト作ってメインクラス作りました。 Project:Lab Inside Status System = Liss = リス = Hoernchen みたいな感じです。 Hoernchen はドイツ語で リス です。 不満があれば言ってください。

---
+++ src/jp/ac/oit/sclab/hoernchen/main/LissMain001.java
@@ -0,0 +1,13 @@
+package jp.ac.oit.sclab.hoernchen.main;
+
+public class LissMain001 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
