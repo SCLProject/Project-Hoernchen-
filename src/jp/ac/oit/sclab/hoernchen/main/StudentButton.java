bdea4e3e2d69bddcab7e94f592c514c73c43ce98
Masanori Kato <nitaka_masa@gmail.com>
2012-05-17 12:52:05 +0900

    ぼたんついあｋ

---
+++ src/jp/ac/oit/sclab/hoernchen/main/StudentButton.java
@@ -0,0 +1,68 @@
+package jp.ac.oit.sclab.hoernchen.main;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Region;
+
+public class StudentButton extends Region  implements Initializable{
+
+	@FXML TextField text_name;
+	@FXML TextField text_time;
+	@FXML TextField text_seat;
+	@FXML TextField text_access;
+
+
+	FXMLLoader fxmlLoader;
+
+
+
+	public StudentButton(){
+
+		URL location = getClass().getResource("");
+
+
+
+
+
+
+	}
+
+
+
+
+
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO 自動生成されたメソッド・スタブ
+
+
+
+
+
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
